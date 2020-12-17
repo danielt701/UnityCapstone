72ec8a14b456f48da1f8a2fdc33723a697e0a5ec
Tborchev <trentborchevsky@gmail.com>
2020-12-17 17:27:27 -0500

    shield implemented
    need timer and hit detection

---
+++ Assets/Scripts/Shield.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+    public GameObject shield;
+
+    private bool activeShield;
+
+    private void Start()
+    {
+        activeShield = false;
+        shield.SetActive(false);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "shield")
+        {
+            if(!activeShield)
+            {
+                shield.SetActive(true);
+                activeShield = true;
+
+            }
+            else
+            {
+                shield.SetActive(false);
+                activeShield = false;
+            }
+        }
+    }
+
+    public bool ActiveShield
+    {
+        get
+        {
+            return activeShield;
+        }
+        set
+        {
+            activeShield = value;
+        }
+    }
+}
