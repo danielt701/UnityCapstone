479726c8df12e6a57c75b4e1625eb553c4a018e0
Tborchev <trentborchevsky@gmail.com>
2021-01-03 14:05:13 -0500

    EnemyFollowPlayerIn
    still follows no matter what, need to make it only within certain range

---
+++ Assets/Scripts/FollowTimer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTimer : MonoBehaviour
+{
+    public Transform Player;
+    int MoveSpeed = 3;
+    int MaxDist = 10;
+    int MinDist = 2;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(Player);
+
+        if(Vector3.Distance(transform.position, Player.position) >= MinDist)
+        {
+            transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+        }
+
+
+        if(Vector3.Distance(transform.position, Player.position) <= MaxDist)
+        {
+
+        }
+    
+    }
+}
