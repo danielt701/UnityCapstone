3efdbe7a43c4907139274f87bd38bfaff5dfb081
Tborchev <trentborchevsky@gmail.com>
2021-01-11 14:22:41 -0500

    Back to Normal
    hopping everything is right

---
+++ Assets/Scripts/PlayerPosition.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerPosition : MonoBehaviour
+{
+    private GameCheckpoint gm;
+
+    void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameCheckpoint>();
+        transform.position = gm.lastCheckPointPos;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Tab))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+}
