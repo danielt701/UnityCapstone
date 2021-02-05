368dbe21282ceb0ed4e21795bb5f20b5d0899e3c
Tborchev <trentborchevsky@gmail.com>
2021-02-04 21:43:19 -0500

    Dialogue Box Popup
    It works from anywhere right now, using E to bring it up.

---
+++ Assets/TalkPopup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TalkPopup : MonoBehaviour
+{
+
+    public string popUp;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            PopupSystem pop = GameObject.FindGameObjectWithTag("GameManager").GetComponent<PopupSystem>();
+            pop.PopUp(popUp);
+        }
+    }
+}
