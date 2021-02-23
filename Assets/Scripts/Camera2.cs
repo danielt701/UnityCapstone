9d6cf0b67665e70f59fdb508130d0ced0d546138
Tborchev <trentborchevsky@gmail.com>
2021-02-23 18:25:51 -0500

    Vet Level Start/moving platforms/chair push
    started putting in the actions for level 2, need artwork though cause none of it is in there so far.

---
+++ Assets/Scripts/Camera2.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera2 : MonoBehaviour
+{
+    [SerializeField]
+
+    private Transform targetToFollow;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.Clamp(targetToFollow.position.x, -24.7f, 155.2f),
+                                         Mathf.Clamp(targetToFollow.position.y, -0.8f, 6.8f),
+                                         transform.position.z);
+    }
+}
