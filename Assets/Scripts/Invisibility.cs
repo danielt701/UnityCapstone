78de12eb3c1bd84217f87f31afe00b31f4f1b150
Tborchev <trentborchevsky@gmail.com>
2021-03-01 20:18:31 -0500

    Revert "Revert "fix""
    This reverts commit 7117507af0c4c30710dc1ff41451dbce4a2b5b11.

--- Assets/Scripts/Invisibility.cs
+++ Assets/Scripts/Invisibility.cs
@@ -10,6 +10,9 @@ public class Invisibility : MonoBehaviour
     private float activationtime;
     private bool invisible;
 
+    public GameObject vet;
+    public GameObject enemy;
+
 
     // Start is called before the first frame update
     void Start()
@@ -29,17 +32,30 @@ public class Invisibility : MonoBehaviour
             invisible = false;
             col.a = 1;
             character.color = col;
+            vet.GetComponent<Collider2D>().enabled = true;
         }
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
+
+
         if(other.tag == "Invisible")
         {
+        
             invisible = true;
             activationtime = 0;
             col.a = .2f;
             character.color = col;
+
+            // for hiding player damage
+
+            vet.GetComponent<Collider2D>().enabled = false;
+
         }
+
+        
     }
+
+
 }
