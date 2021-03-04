6e0698d4784e5a648aba2fa41e60a715459da2cf
Tborchev <trentborchevsky@gmail.com>
2021-03-04 18:44:27 -0500

    Level3/Camera-in/enemiesStart
    beginning stuff for level 3

---
+++ Assets/Scripts/Camera3.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera3 : MonoBehaviour
+{
+    [SerializeField]
+
+    private Transform targetToFollow;
+   
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.Clamp(targetToFollow.position.x, -.20f, 226.7f),
+                                          Mathf.Clamp(targetToFollow.position.y, -2f, 87f),
+                                          transform.position.z);
+    }
+}
---
+++ Assets/Scripts/ItemSpawn.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawn : MonoBehaviour
+{
+    public Transform Spawnpoint;
+    public GameObject Prefab;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Instantiate(Prefab, Spawnpoint.position, Spawnpoint.rotation);
+    }
+
+}
