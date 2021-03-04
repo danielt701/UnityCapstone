b2467a0c147f6f8aaff5f85af86a9d1c94c767b3
Tborchev <trentborchevsky@gmail.com>
2021-03-04 15:05:41 -0500

    Level3Start
    adding charging enemy and placements

---
+++ Assets/Scripts/ChargeScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChargeScript : MonoBehaviour
+{
+
+    private Transform player;
+    private float distance;
+    public float moveSpeed;
+    public float howclose;
+
+    private float rotationX;
+    private float rotationY;
+
+    [SerializeField] private Animator myAnimationController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        
+    }
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+       // distance = Vector2.Distance(player.position, transform.position);
+
+
+      /*  if(distance <= howclose)
+        {
+            transform.LookAt(player);
+            anim.Play("ChargingEnemy");
+        }
+        */
+       
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            myAnimationController.SetBool("DoCharge2", true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            myAnimationController.SetBool("DoCharge2", false);
+        }
+    }
+}
