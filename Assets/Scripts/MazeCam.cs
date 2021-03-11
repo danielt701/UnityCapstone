23ccd1439084dd17acd9ea81891a15f7dcff9350
Tborchev <trentborchevsky@gmail.com>
2021-03-11 15:46:42 -0500

    MazeScene

---
+++ Assets/Scripts/MazeCam.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeCam : MonoBehaviour
+{
+    [SerializeField]
+
+    private Transform targetToFollow;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.Clamp(targetToFollow.position.x, -4.64f, 3.5f),
+                                          Mathf.Clamp(targetToFollow.position.y, -7.15f, 6.60f),
+                                          transform.position.z);
+    }
+}
