0f0d8a3d57d3c3b06dcb286f66d36e091506b7b6
Tborchev <trentborchevsky@gmail.com>
2021-03-25 16:19:03 -0400

    New Challenge and Maze Fixes

---
+++ Assets/Scripts/Camera/DodgeCam.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DodgeCam : MonoBehaviour
+{
+    [SerializeField]
+
+    private Transform targetToFollow;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.Clamp(targetToFollow.position.x, -0.31f, 67f),
+                                         Mathf.Clamp(targetToFollow.position.y, 0.02f, 0.16f),
+                                         transform.position.z);
+    }
+}
---
+++ Assets/Scripts/MissleFollow.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class MissleFollow : MonoBehaviour
+{
+
+    public Transform target;
+
+    private Rigidbody2D rb;
+
+    public float speed = 5f;
+
+    public float rotateSpeed = 200f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 direction = (Vector2)target.position - rb.position;
+
+        direction.Normalize();
+
+        float rotateAmount = Vector3.Cross(direction, transform.up).z;
+
+        rb.angularVelocity = -rotateAmount * rotateSpeed;
+
+        rb.velocity = transform.up * speed;
+    }
+}
